695b79d2cb337241a50ce0a410b9b6eb6780327c
minh <Lminhphu0304@gmail.com>
2021-07-03 19:18:51 +1200

    Vibes 👁️ 👄 👁️

--- index.ts
+++ index.ts
@@ -5,18 +5,19 @@ import { route } from './routes';
 const port = 3000;
 
 export const shitHTML = `
-<div style='display: block !important; position: fixed !important; top: 1rem !important; left: 1rem !important; border-radius: 8px !important; background-color: white !important; z-index: 90000000 !important; padding: 1rem !important;box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.08) !important; '>
-<h1 style='font-size: 1.5rem !important; font-family: sans-serif !important; color: black !important; margin-bottom: 1rem !important;'>Vibe AI </h1>
-<ul style='font-size: 1rem !important; font-family: sans-serif !important; color: black !important;'>
+<script src="https://unpkg.com/react-dom@17/umd/react-dom.production.js" crossorigin></script>
+<div style='display: block !important; position: fixed !important; top: 1rem !important; right: 1rem !important; border-radius: 8px !important; background-color: white !important; z-index: 90000000 !important; padding: 1rem !important;box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.08) !important; '>
+<h1 style='font-size: 1.5rem !important; font-family: "comic sans ms" !important; color: black !important; margin-bottom: 1rem !important;'>Vibe AI </h1>
+<ul style='font-size: 1rem !important; font-family: "comic sans ms" !important; color: black !important;'>
 <li>step 1: Fire your engineering, design and product teams</li>
 <li>step 2: Type in the product you want in the field below</li>
 <li>step 3: Hit submit and let our AI build your product</li>
 </ul>
 <form method="POST" action="/template" style='margin-top: 1rem !important!'>
-  <textarea style='font-family: sans-serif !important' name="content" placeholder="Enter your 1M dollar idea here"  rows="5" cols="50" required></textarea>
+  <textarea style='font-family: "comic sans ms" !important' name="content" placeholder="Enter your 1M dollar idea here"  rows="5" cols="50" required></textarea>
   <br>
   <br>
-  <button type="submit" style='font-family: sans-serif !important'>submit</button>
+  <button type="submit" style='font-family: "comic sans ms" !important'>submit</button>
 </form>
 </div>
 `
