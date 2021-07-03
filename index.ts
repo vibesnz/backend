58b9ef2bcef59006742d6f3e7406273d957c83e0
minh <Lminhphu0304@gmail.com>
2021-07-03 18:14:29 +1200

    Vibes 👁️ 👄 👁️

--- index.ts
+++ index.ts
@@ -5,10 +5,23 @@ import { route } from './routes';
 
 const port = 3000;
 
+const shitHTML = `
+<html>
+  <body>
+    <form method="POST" action="/template">
+      <input name="content"/>
+    </form>
+  </body>
+</html>
+`
+
 const server = Express();
 server.use(cors());
 server.use(json());
-
+server.use(Express.static('public'))
 server.use('/api', route);
+server.get('/', (_, res) => {
+  res.send(shitHTML)
+})
 
 server.listen(port, () => { console.log("Good vibes only"); });
