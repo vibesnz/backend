75790158f928279fafd77b3360e3f0b1b02b22f8
minh <Lminhphu0304@gmail.com>
2021-07-03 18:02:27 +1200

    Vibes 👁️ 👄 👁️

--- routes/index.ts
+++ routes/index.ts
@@ -1,11 +1,15 @@
 import { Router } from 'express';
 
+import { getTemplateHandler, postTemplateHandler } from './template';
+
 const route = Router();
 
 route.get('/heatlhCheck', (_, res) => {
-  res.json({ success: true }).send();
+  res.json({ success: true }).end();
 });
 
+route.get('/template', getTemplateHandler);
+route.post('/template', postTemplateHandler);
 
 export {
   route
