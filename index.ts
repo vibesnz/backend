a4f5a0376f4796d391decf985e601eca9a51e402
Walter Lim <waltzaround@users.noreply.github.com>
2021-07-03 18:57:06 +1200

    Update index.ts

--- index.ts
+++ index.ts
@@ -8,15 +8,15 @@ export const shitHTML = `
 <div style='display: block !important; position: fixed !important; top: 1rem !important; left: 1rem !important; border-radius: 8px !important background-color: white !important; z-index: 90000000 !important; padding: 1rem !important; '>
 <h1 style='font-size: 1.5rem !important; font-family: sans-serif !important; color: black !important; margin-bottom: 1rem !important;'>Vibe AI </h1>
 <ul style='font-size: 1rem !important; font-family: sans-serif !important; color: black !important;'>
-<li>step 1: fire your engineering, design and product teams</li>
-<li>step 2: type in the product you want in the field below</li>
-<li>step 3: hit submit</li>
+<li>step 1: Fire your engineering, design and product teams</li>
+<li>step 2: Type in the product you want in the field below</li>
+<li>step 3: Hit submit and let our AI build your product</li>
 </ul>
 <form method="POST" action="/template" style='margin-top: 1rem !important!'>
-  <textarea name="content" placeholder="Enter your 1M dollar idea here"  rows="5" cols="50" required></textarea>
+  <textarea style='font-family: sans-serif !important' name="content" placeholder="Enter your 1M dollar idea here"  rows="5" cols="50" required></textarea>
   <br>
   <br>
-  <button type="submit">submit</button>
+  <button type="submit" style='font-family: sans-serif !important'>submit</button>
 </form>
 </div>
 `
