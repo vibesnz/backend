b4fdca323add97143ebda68ca00636422517afc9
minh <Lminhphu0304@gmail.com>
2021-07-03 18:38:21 +1200

    Vibes 👁️ 👄 👁️

--- index.ts
+++ index.ts
@@ -5,9 +5,17 @@ import { route } from './routes';
 const port = 3000;
 
 export const shitHTML = `
+<h1>Vibe City</h1>
+<ul>
+<li>step 1: fire your engineering, design and product teams</li>
+<li>step 2: type in the product you want in the field below</li>
+<li>step 3: hit submit</li>
+</ul>
 <form method="POST" action="/template">
-  <textarea name="content"></textarea>
-  <button type="submit">Build this</button>
+  <textarea name="content" placeholder="Enter your 1M dollar idea here"  rows="5" cols="50" required></textarea>
+  <br>
+  <br>
+  <button type="submit">submit</button>
 </form>
 `
 
